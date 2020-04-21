1b9f27175181c7c9b97ae584e7d4b59ed230d2b8
PC <PC@DESKTOP-IRJ7K24>
2020-04-20 23:31:59 -0300

    Ariel Lobos: Funcionalidad de la ventana ejercicio 1

--- TP3_GRUPO_3/src/tp3_grupo3/VentanaEjercicio1.java
+++ TP3_GRUPO_3/src/tp3_grupo3/VentanaEjercicio1.java
@@ -4,6 +4,9 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
 
 public class VentanaEjercicio1 extends JFrame{
 
@@ -57,10 +60,79 @@ public class VentanaEjercicio1 extends JFrame{
 		lblDatosIngresados.setBounds(30, 314, 170, 14);
 		getContentPane().add(lblDatosIngresados);
 		
-		JLabel lblMostrarDatos = new JLabel("");
-		lblMostrarDatos.setBounds(205, 314, 269, 14);
-		getContentPane().add(lblMostrarDatos);
+		JLabel lblMostrarDatos1 = new JLabel("");
+		lblMostrarDatos1.setBounds(205, 314, 269, 14);
+		getContentPane().add(lblMostrarDatos1);
+		
+		JLabel lblMostrarDatos2 = new JLabel("");
+		lblMostrarDatos2.setBounds(205, 340, 269, 14);
+		getContentPane().add(lblMostrarDatos2);
+		
+		JLabel lblMostrarDatos3 = new JLabel("");
+		lblMostrarDatos3.setBounds(205, 365, 269, 14);
+		getContentPane().add(lblMostrarDatos3);
+		
+		JLabel lblMostrarDatos4 = new JLabel("");
+		lblMostrarDatos4.setBounds(205, 390, 269, 14);
+		getContentPane().add(lblMostrarDatos4);
 
+		
+		
+		btnMostrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				boolean incompleto=false;
+				
+				if(txtNombre.getText().isEmpty()) {
+					txtNombre.setBackground(Color.RED);
+					incompleto=true;
+				}else {
+					txtNombre.setBackground(Color.WHITE);
+				}
+									
+				if(txtApellido.getText().isEmpty()) {
+					txtApellido.setBackground(Color.RED);
+					incompleto=true;
+				}else {
+					txtApellido.setBackground(Color.WHITE);
+				}
+				
+				if(txtTelefono.getText().isEmpty()) {
+					txtTelefono.setBackground(Color.RED);
+					incompleto=true;
+				}else {
+					txtTelefono.setBackground(Color.WHITE);
+				}
+				
+				if(txtFechaNac.getText().isEmpty()) {
+					txtFechaNac.setBackground(Color.RED);
+					incompleto=true;
+				}else {
+					txtFechaNac.setBackground(Color.WHITE);
+				}
+				
+				if(incompleto==false) {
+					lblMostrarDatos1.setText("Nombre: "+txtNombre.getText());
+					lblMostrarDatos2.setText("Apellido: "+txtApellido.getText());
+					lblMostrarDatos3.setText("Telefono: "+txtTelefono.getText());
+					lblMostrarDatos4.setText("Fecha Nac.: "+txtFechaNac.getText());
+					
+					txtNombre.setText("");
+					txtApellido.setText("");
+					txtTelefono.setText("");
+					txtFechaNac.setText("");
+				}else {
+					lblMostrarDatos1.setText("");
+					lblMostrarDatos2.setText("");
+					lblMostrarDatos3.setText("");
+					lblMostrarDatos4.setText("");
+				}
+				
+				
+			}
+		});
+		
+		
 	}
 	
 	public void cambiarVisibilidad(boolean estado) {
